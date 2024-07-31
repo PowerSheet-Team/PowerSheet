1e4970a5dc4ea31200a6326f02ad0945897feec2
ColorsWind <14761584+colorswind@users.noreply.github.com>
2024-08-01 00:22:13 +0800

    update to RyzenAI 1.2

--- platform/llm/impl/llm_npu.py
+++ platform/llm/impl/llm_npu.py
@@ -1,22 +1,42 @@
 from __future__ import annotations
 
+from transformers import PreTrainedTokenizerFast
+from pre_quant import apply_awq
+from quantizer import real_quantize_model_weight
 from .llm_hf import LLM_HF
-from .amd import LlamaForCausalLM
-from transformers import AutoTokenizer
-import qlinear
+from llm_models import HF_LLAMA_3_8B_INSTRUCT, Q_LLAMA_3_8B_INSTRUCT, AWQ_LLAMA_3_8B_INSTRUCT, HF_LLAMA_3_8B_INSTRUCT_SHORT_NAME
+from llm_eval import (
+    LlamaModelEval,
+)
+from ryzenai_llm_engine import RyzenAILLMEngine, TransformConfig
+from transformers import PreTrainedTokenizerFast
+from ryzenai_llm_quantizer import QuantConfig, RyzenAILLMQuantizer
 
 import torch
 
 class LLM_NPU(LLM_HF):
     def __init__(self, model_name: str, q_name: str, system_prompt: str) -> None:
-        from llm.impl.amd import LlamaAttention, LlamaForCausalLM, LlamaFlashAttention
-        model: LlamaForCausalLM = torch.load(q_name)
-        tokenizer = AutoTokenizer.from_pretrained(model_name)
+        model: LlamaModelEval = torch.load(q_name)
+        print(model)
+        tokenizer = PreTrainedTokenizerFast.from_pretrained(model_name)
         
-        for n, m in model.named_modules():
-            if isinstance(m, qlinear.QLinearPerGrp):
-                print(f"Preparing weights of layer : {n}")
-                m.device = "aie"
-                m.quantize_weights()
+        transform_config = TransformConfig(
+            flash_attention_plus=True,
+            fast_mlp=True,
+            fast_attention=False,
+            precision="w4abf16",
+            model_name=HF_LLAMA_3_8B_INSTRUCT_SHORT_NAME,
+            target="aie",
+            w_bit=4,
+            group_size=128,
+            profilegemm=False,
+            profile_layer=False,
+            mhaops="all",
+        )
+
+        model = RyzenAILLMEngine.transform(model, transform_config)
+        model = model.to(torch.bfloat16)
+        print(model)
+        model.eval()
         super().__init__(model, tokenizer, system_prompt)
 
--- platform/llm_models.py
+++ platform/llm_models.py
@@ -5,3 +5,4 @@ import os
 HF_LLAMA_3_8B_INSTRUCT = os.path.join(os.path.dirname(__file__), '..', 'models', 'Meta-Llama-3-8B-Instruct')
 AWQ_LLAMA_3_8B_INSTRUCT = os.path.join(os.path.dirname(__file__), '..', 'models', 'llama3-instruct-8b-w4-g128.pt')
 Q_LLAMA_3_8B_INSTRUCT = os.path.join(os.path.dirname(__file__), '..', 'models', 'Meta-Llama-3-8B-Instruct-quantized.ckpt')
+os.environ['AWQ_CACHE'] = os.path.join(os.path.dirname(__file__), '..', 'models')
