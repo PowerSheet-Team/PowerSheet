52c1c6a2714b8eef9f9934497eef63d35b61b1ea
ColorsWind <14761584+colorswind@users.noreply.github.com>
2024-08-01 11:51:15 +0800

    update HF_LLAMA_3_8B_INSTRUCT_SHORT_NAME

--- platform/llm_models.py
+++ platform/llm_models.py
@@ -5,4 +5,5 @@ import os
 HF_LLAMA_3_8B_INSTRUCT = os.path.join(os.path.dirname(__file__), '..', 'models', 'Meta-Llama-3-8B-Instruct')
 AWQ_LLAMA_3_8B_INSTRUCT = os.path.join(os.path.dirname(__file__), '..', 'models', 'llama3-instruct-8b-w4-g128.pt')
 Q_LLAMA_3_8B_INSTRUCT = os.path.join(os.path.dirname(__file__), '..', 'models', 'Meta-Llama-3-8B-Instruct-quantized.ckpt')
+HF_LLAMA_3_8B_INSTRUCT_SHORT_NAME = 'Meta-Llama-3-8B-Instruct'
 os.environ['AWQ_CACHE'] = os.path.join(os.path.dirname(__file__), '..', 'models')
